0e2ad9466e87605729d52851924980e2466712fb
noone <kolosochek@gmail.com>
2023-08-06 07:49:25 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Box, Button, FormGroup, Grid, TextField} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="b-app-wrapper">
+      <Grid container className="b-app">
+          <Grid item>
+              <Box component={"header"} className="b-header">
+                  <img src={logo} alt={"app logo"}  height={200} />
+              </Box>
+              <Box component={"main"} className="b-main">
+                  <form>
+                      <FormGroup>
+                          <TextField label={"Username"} />
+                          <TextField label={"Password"} />
+                          <Button type={"submit"}>Log in</Button>
+                      </FormGroup>
+                  </form>
+              </Box>
+              <Box component={"footer"} className="b-footer">
+
+              </Box>
+          </Grid>
+      </Grid>
+    </Box>
   );
 }
 
