ab1270b8ceeb334ac40681cf4bd1799b4fdec4a1
noone <kolosochek@gmail.com>
2023-08-06 12:31:59 +0200

    router refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Route, Routes} from "react-router-dom";
 import {IndexPage} from "./pages/index/IndexPage";
-import {ErrorPage} from "./pages/error/ErrorPage";
-import {AboutPage} from "./pages/about/AboutPage";
 
 function App() {
   return (
-      <Routes>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <IndexPage />
   );
 }
 
