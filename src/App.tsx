966a6decc4d2bf879fb93ce50f61b2b4dbe8f7c7
noone <kolosochek@gmail.com>
2023-08-06 15:40:50 +0200

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
 import {IndexPage} from "./pages/index/IndexPage";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {AboutPage} from "./pages/about/AboutPage";
 
 
 function App() {
     return (
-        <Router>
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<IndexPage/>}/>
                 <Route path="/about" element={<AboutPage/>}/>
+                <Route path="*" element={<IndexPage/>}/>
             </Routes>
-        </Router>
+        </BrowserRouter>
 
     );
 }
